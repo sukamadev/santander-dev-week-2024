a02d9d3adf54f80b40b902e804a85277350282ee
sukama <sukamasarmento@gmail.com>
2024-06-09 15:34:15 -0300

    Initial commit

---
+++ santander-dev-week-2024/src/main/java/me/dio/santander_dev_week_2024/SantanderDevWeek2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dev_week_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2024Application.class, args);
+	}
+
+}
